c76941c2cd3f26cb9a1c3066f65090a2bce2be8e
Mariazd19 <darouamaria3@gmail.com>
2023-06-15 14:33:22 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints a line with pute function
+ * Return: 0 (Success)
+*/
+
+int main(void)
+	
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
