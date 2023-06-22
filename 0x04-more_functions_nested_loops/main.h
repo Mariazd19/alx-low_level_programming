1fc78a99c6a02b4fd7d4cc841f8e3f2c31d6dd0e
Mariazd19 <darouamaria3@gmail.com>
2023-06-22 09:16:57 -0700

    1-isdigit.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,10 +1,3 @@
-#ifndef FILE_MAIN
-#define FILE_MAIN
-
-int _putchar(char c);
-int _isupper(int c);
-int _isdigit(int c);
-int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
@@ -13,5 +6,7 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
-
-#endif
+int _putchar(char);
+int _isupper(int);
+int _isdigit(int);
+int mul(int, int);
