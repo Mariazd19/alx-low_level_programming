c8adfedd532b5434d0daefdc6e308a8b4223eeb2
Mariazd19 <darouamaria3@gmail.com>
2023-07-03 11:43:30 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,23 +1,26 @@
-#include "holberton.h"
+/*
+ * File: 0-memset.c
+ * Auth: not
+ */
+
+#include "main.h"
 
 /**
- * _memset - Write a function that fills memory with a constant byte
- * The _memset() function fills the first n bytes of the memory area pointed to
- * by s with the constant byte b
- *
- * @s: This is the output and the return
- * @b: This is the this is the input string
- * @n: This is the number of bytes
- *
- * Return: A pointer to the memory area s
- */
+*_memset - The _memset() function fills
+* the first n bytes of the memory area
+* pointed to by s with the constant byte b
+*@s:target
+*@b: constant byte
+*@n:number of byte
+*Return: returns new value of target
+*/
+
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int count;
-
-	for (count = 0; count < n ; count++)
+	while (n)
 	{
-		s[count] = b;
+		s[n - 1] = b;
+		n--;
 	}
 	return (s);
 }
